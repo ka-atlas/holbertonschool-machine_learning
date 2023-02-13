d3bcc073ab5fbed2e5163b274a88886730b39e65
kush-holberton <kush.aswani@holbertonschool.com>
2023-02-13 14:16:27 -0600

    created the 8th file

---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def mat_mul(mat1, mat2):
+    if len(mat1[1])!=len(mat2):
+        return None
+    result = []
+
+    return_mat = [[0 for x in range(len(mat2[0]))] for y in range(len(mat1))]
+
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                return_mat[i][j] += mat1[i][k] * mat2[k][j]
+
+    return return_mat
