35b68454964eb6a8a5010bf164fe0ecd157fdf40
kush-holberton <kush.aswani@holbertonschool.com>
2023-02-13 13:31:53 -0600

    updated the 5th file

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 def add_matrices2D(mat1, mat2):
-    return [[mat1[i][j]+mat2[i][j] for j in range(len(mat1))]
+    return [[mat1[j][i]+mat2[j][i] for j in range(len(mat1))]
                       for i in range(len(mat1[0]))]
