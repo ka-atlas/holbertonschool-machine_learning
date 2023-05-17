935e4c9883f67024a72b17d1d1dccc090bf5eee8
kush-holberton <kush.aswani@holbertonschool.com>
2023-05-17 14:10:52 -0500

    added solution for 9-sum_total

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+"""Sigma Equation"""
+
+
 def summation_i_squared(n):
-    result = 0
-    for i in range(n+1):
-        result += n**2
-    return result
+    """Addition With Sigma"""
+    if isinstance(n, int):
+        return recursive_summation(n, 1, 0)
+    else:
+        return None
+
+
+def recursive_summation(n, x, i):
+    """helper function"""
+    if x <= n:
+        i = i + x ** 2
+        return recursive_summation(n, x + 1, i)
+    else:
+        return i
