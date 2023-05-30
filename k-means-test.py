655d659248debea2751a2c3a538ffced7550e33d
kush-holberton <124093082+kush-holberton@users.noreply.github.com>
2023-05-30 13:13:26 -0500

    Create k-means-test.py

---
+++ k-means-test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sklearn.cluster
+"""
+Perform k-means on a dataset. Only import allowed
+"""
+
+
+def kmeans(X, k):
+    """
+    X: numpy.ndarray of shape (n, d) containing the dataset
+    k: number of clusters
+    Returns: C, clss
+        C: numpy.ndarray shape (k, d) containing the centroid means
+        clss: numpy.ndarray shape (n,) containing the index of the cluster
+            C that each data point belongs to
+    """
+    C, clss, _ = sklearn.cluster.k_means(X, k, n_init='auto')
+
+    return C, clss
