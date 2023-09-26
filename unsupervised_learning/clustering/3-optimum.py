456f7d34215ddc4c7dc3cee913241546b92e213c
kush-holberton <124093082+kush-holberton@users.noreply.github.com>
2023-09-26 09:39:19 -0500

    Create 3-optimum.py

---
+++ unsupervised_learning/clustering/3-optimum.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Imports
+"""
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """
+    Tests for the optimum number of clusters by variance
+    """
+
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None, None
+
+    if not isinstance(kmin, int) or kmin < 1:
+        return None, None
+
+    if kmax is None:
+        kmax = X.shape[0]
+
+    if not isinstance(kmax, int) or kmax <= 0 or kmin >= kmax:
+        return None, None
+
+    if not isinstance(iterations, int) or iterations < 1:
+        return None, None
+
+    results = []
+    d_vars = []
+
+    C, clss = kmeans(X, k, iterations)
+    var = variance(X, C)
+    results.append((klusters, klss))
+    d_vars.append(0.0)
+  
+    for k in range(kmin+1, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        results.append((C, clss))
+        d_vars.append(abs(var - variance(X, C)))
+
+    return results, d_vars
